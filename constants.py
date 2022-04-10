58bfe498e50cc961b6416bbf541b225857d77e73
H. C. Das <51406386+hcdas@users.noreply.github.com>
2022-04-10 18:23:47 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,28 @@
+import math
+
+G  = 6.6730831e-8
+c  = 2.99792458e10
+MeV_fm3_to_pa = 1.6021766e35
+c_km = 2.99792458e5 # km/s
+mN = 1.67e-24 # g
+mev_to_ergs = 1.602176565e-6
+fm_to_cm = 1.0e-13
+ergs_to_mev = 1.0/mev_to_ergs
+cm_to_fm = 1.0/fm_to_cm
+Msun = 1.988435e33
+MeV_fm3_to_pa_cgs = 1.6021766e33
+km_to_mSun = G/c**2
+
+hbarc3 = 197.32700288295746**3
+
+nucleon_mass = 938.04
+
+pi = math.pi
+
+# Conversion factors (see Glendenning, Compact stars)
+# 1 MeVfm^-3 = 1.3234e-6 km^-2
+MeVfm_3Tokm_2 = 1.3234e-6;
+km_2ToMeVfm_3 = 1.0/MeVfm_3Tokm_2;
+# 1 M_odot = 1.4766 km
+ModotTokm = 1.4766;
+kmToModot = 1.0/ModotTokm;
